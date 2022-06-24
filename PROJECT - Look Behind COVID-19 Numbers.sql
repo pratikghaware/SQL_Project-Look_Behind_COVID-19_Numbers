b27ff46a4416a3990243f881c27b3115dc74f066
Pratik Suresh Ghaware <104768112+pratikghaware@users.noreply.github.com>
2022-06-24 14:25:01 +0530

    Delete PROJECT - Look Behind COVID-19 Numbers.sql

--- PROJECT - Look Behind COVID-19 Numbers.sql
+++
@@ -1,8 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [continent]
-      ,[location]
-      ,[date]
-      ,[population]
-      ,[new_vaccinations]
-      ,[RollingPeopleVaccinated]
-  FROM [covid_project].[dbo].[percent_population_vaccinated]
